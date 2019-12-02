ae9b9f7287f4e7fed81f4d8a15a507ea1d20edf0
Puttzer <carragher@live.se>
2019-12-02 18:05:36 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const express = require('express');
+const express = require('express'); //require express
+
 const app = express();
 
-app.use('/static', express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+
+
+app.use('/static', express.static('public')); //serve a static files sush as images,cssfiles, and js
+
+app.get('/', (req, res) => {  // create a route an use callback function
+    res.sendFile(__dirname + '/index.html');
+
 });
 
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/public/about.html');
+})
+
+app.get('/form', (req, res) => {
+    res.sendFile(__dirname + '/public/form.html');
+})
 
 
-app.listen('3000');
+
+
+
+
+app.listen(3000);  // start the applocation at the port 8080
+
